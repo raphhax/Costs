d1f65a372296d3ccdd5bb6fb1a5ad00d285c34b0
Raphael Henrique <raphaelhenriquedev@gmail.com>
2023-06-04 00:16:31 -0300

    ataulizacaoesPagians

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,4 +1,9 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
 
 function App() {
   return (
